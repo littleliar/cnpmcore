e2bca5bed7ec6893210e0bf5db5d5809f1c84741
fengmk2 <fengmk2@gmail.com>
2021-12-19 19:19:24 +0800

    f

--- test/SyncAllPackages.ts
+++ test/SyncAllPackages.ts
@@ -14,10 +14,28 @@ async function createTask(url: string) {
 async function main() {
   const data = await load();
   console.log('Total %d packages', data.packageNames.length);
-  const lastIndex = 30;
+  const lastIndex = 22183;
   for (const [ index, fullname ] of data.packageNames.entries()) {
     if (index < lastIndex) continue;
     let success = false;
+    try {
+      const url = `https://registry.npmmirror.com/${fullname}/sync?sync_upstream=true`;
+      const result = await createTask(url);
+      const data = result.data;
+      if (data && data.logId) {
+        const logUrl = `https://registry.npmmirror.com/${fullname}/sync/log/${data.logId}`;
+        console.log('[%s] %s, status: %s, log: %s', index, fullname, result.status, logUrl);
+      } else {
+        console.log('[%s] %s, status: %s, data: %j', index, fullname, result.status, data);
+      }
+      success = true;
+    } catch (err) {
+      console.error('[%s] %s, error: %s', index, fullname, err);
+    }
+
+    await setTimeout(1000);
+    if (success) continue;
+
     try {
       const url = `https://r2.cnpmjs.org/-/package/${fullname}/syncs`;
       const result = await createTask(url);
